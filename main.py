eff7af21e7c320cf9e9b06d87e327c14ef3004fe
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-03-11 15:39:53 +0000

    Made all work

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+class Person:
+
+  def __init__(self):
+    r = requests.get("https://randomuser.me/api/")
+    res = r.json()
+    self.name = res ["results"][0]["name"]["first"]
+    self.surname = res ["results"][0]["name"]["last"]
+    self.age = res ["results"][0]["dob"]["age"]
+
+
+random_person = Person()
+print(random_person.name)
+print(random_person.surname)
+print(random_person.age)
